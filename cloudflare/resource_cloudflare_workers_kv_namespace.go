807bdf7c3967b6666e887723b520799272286d92
Andrew Plunk <aplunk@cloudflare.com>
2018-12-17 07:19:02 -0600

    WorkersKV: stub out namespace provider

---
+++ cloudflare/resource_cloudflare_workers_kv_namespace.go
@@ -0,0 +1,36 @@
+package cloudflare
+
+import "github.com/hashicorp/terraform/helper/schema"
+
+func resourceCloudflareWorkersKVNamespace() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceCloudflareWorkersKVNamespaceCreate,
+		Read:   resourceCloudflareWorkersKVNamespaceRead,
+		Update: resourceCloudflareWorkersKVNamespaceUpdate,
+		Delete: resourceCloudflareWorkersKVNamespaceDelete,
+		Importer: &schema.ResourceImporter{
+			State: resourceCloudflareWorkersKVNamespaceImport,
+		},
+		Schema: map[string]*schema.Schema{},
+	}
+}
+
+func resourceCloudflareWorkersKVNamespaceCreate(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceCloudflareWorkersKVNamespaceRead(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceCloudflareWorkersKVNamespaceUpdate(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceCloudflareWorkersKVNamespaceDelete(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceCloudflareWorkersKVNamespaceImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	return nil, nil
+}
