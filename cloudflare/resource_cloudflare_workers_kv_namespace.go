4d729df9206d1e4735aff4bbd3136f700ec932b2
Andrew Plunk <aplunk@cloudflare.com>
2018-12-24 00:08:17 -0600

    WorkersKV: implement namespace read

--- cloudflare/resource_cloudflare_workers_kv_namespace.go
+++ cloudflare/resource_cloudflare_workers_kv_namespace.go
@@ -10,16 +10,22 @@ import (
 	"github.com/pkg/errors"
 )
 
+const (
+	allKey   = "all"
+	titleKey = "title"
+)
+
 func resourceCloudflareWorkersKVNamespace() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceCloudflareWorkersKVNamespaceCreate,
 		Update: resourceCloudflareWorkersKVNamespaceUpdate,
 		Delete: resourceCloudflareWorkersKVNamespaceDelete,
+		Read:   resourceCloudflareWorkersKVNamespaceRead,
 		Importer: &schema.ResourceImporter{
 			State: resourceCloudflareWorkersKVNamespaceImport,
 		},
 		Schema: map[string]*schema.Schema{
-			"title": &schema.Schema{
+			titleKey: &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
@@ -29,7 +35,7 @@ func resourceCloudflareWorkersKVNamespace() *schema.Resource {
 
 func resourceCloudflareWorkersKVNamespaceCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*cloudflare.API)
-	title := d.Get("title").(string)
+	title := d.Get(titleKey).(string)
 	if title == "" {
 		return fmt.Errorf("missing required title field")
 	}
@@ -42,18 +48,19 @@ func resourceCloudflareWorkersKVNamespaceCreate(d *schema.ResourceData, meta int
 		return err
 	}
 
-	d.SetId(resp.Result.Title)
+	d.SetId(resp.Result.ID)
 	return nil
 }
 
 func resourceCloudflareWorkersKVNamespaceUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*cloudflare.API)
-	id := d.Get("id").(string)
-	title := d.Get("title").(string)
+	title := d.Get(titleKey).(string)
 
-	_, err := client.UpdateWorkersKVNamespace(context.Background(), id,
+	_, err := client.UpdateWorkersKVNamespace(context.Background(), d.Id(),
 		&cloudflare.WorkersKVNamespaceRequest{Title: title},
 	)
+
+	d.Set(titleKey, title)
 
 	if err != nil {
 		return errors.Wrap(err, "error updating worker kv namespace")
@@ -64,9 +71,8 @@ func resourceCloudflareWorkersKVNamespaceUpdate(d *schema.ResourceData, meta int
 
 func resourceCloudflareWorkersKVNamespaceDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*cloudflare.API)
-	id := d.Get("id").(string)
 
-	_, err := client.DeleteWorkersKVNamespace(context.Background(), id)
+	_, err := client.DeleteWorkersKVNamespace(context.Background(), d.Id())
 	if err != nil {
 		if strings.Contains(err.Error(), "HTTP status 404") {
 			return nil
@@ -78,6 +84,42 @@ func resourceCloudflareWorkersKVNamespaceDelete(d *schema.ResourceData, meta int
 	return nil
 }
 
+func makeNamespaceFilter(d *schema.ResourceData) func(string) bool {
+	id := d.Id()
+	switch strings.ToLower(id) {
+	case allKey:
+		return func(_ string) bool { return true }
+	default:
+		return func(nxtID string) bool { return id == nxtID }
+	}
+}
+
+func resourceCloudflareWorkersKVNamespaceRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudflare.API)
+
+	resp, err := client.ListWorkersKVNamespaces(context.Background())
+	if err != nil {
+		if strings.Contains(err.Error(), "HTTP status 404") {
+			return nil
+		}
+
+		return errors.Wrap(err, "error deleting workers kv namespace")
+	}
+
+	filter := makeNamespaceFilter(d)
+
+	for _, namespace := range resp.Result {
+		if !filter(namespace.ID) {
+			continue
+		}
+		data := &schema.ResourceData{}
+		data.SetId(namespace.ID)
+		data.Set(titleKey, namespace.Title)
+		break
+	}
+	return nil
+}
+
 func resourceCloudflareWorkersKVNamespaceImport(d *schema.ResourceData, meta interface{}) (result []*schema.ResourceData, err error) {
 	client := meta.(*cloudflare.API)
 
@@ -86,14 +128,7 @@ func resourceCloudflareWorkersKVNamespaceImport(d *schema.ResourceData, meta int
 		return nil, errors.Wrap(err, "error importing workers kv namespaces")
 	}
 
-	id := d.Id()
-	var filter func(id string) bool
-	switch strings.ToLower(id) {
-	case "all":
-		filter = func(_ string) bool { return true }
-	default:
-		filter = func(nxtID string) bool { return id == nxtID }
-	}
+	filter := makeNamespaceFilter(d)
 
 	for _, namespace := range resp.Result {
 		if !filter(namespace.ID) {
@@ -101,7 +136,8 @@ func resourceCloudflareWorkersKVNamespaceImport(d *schema.ResourceData, meta int
 		}
 		data := &schema.ResourceData{}
 		data.SetId(namespace.ID)
-		data.Set("title", namespace.Title)
+		data.Set(titleKey, namespace.Title)
+		data.SetType("cloudflare_workers_kv_namespace")
 		result = append(result, data)
 	}
 
