ae35fe4df307f270ef3c8abd893bb2e66343d422
Andrew Plunk <aplunk@cloudflare.com>
2018-12-24 02:19:44 -0600

    WorkersKV: create kv implementation

--- cloudflare/resource_cloudflare_workers_kv.go
+++ cloudflare/resource_cloudflare_workers_kv.go
@@ -2,21 +2,25 @@ package cloudflare
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
 
 	"crypto/sha256"
 
 	cloudflare "github.com/cloudflare/cloudflare-go"
 	"github.com/hashicorp/terraform/helper/schema"
+	home "github.com/mitchellh/go-homedir"
 )
 
 func resourceCloudflareWorkersKV() *schema.Resource {
 	return &schema.Resource{
-		Create: nil,
-		Update: nil,
-		Delete: nil,
+		Create: resourceCloudflareWorkersKVCreate,
+		Delete: resourceCloudflareWorkersKVDelete,
 		Read:   resourceCloudflareWorkersKVRead,
+		Update: resourceCloudflareWorkersKVUpdate,
 		Importer: &schema.ResourceImporter{
-			State: nil,
+			State: resourceCloudflareWorkersKVImport,
 		},
 		Schema: map[string]*schema.Schema{
 			"content": {
@@ -60,6 +64,61 @@ func resourceCloudflareWorkersKVRead(d *schema.ResourceData, meta interface{}) e
 		return err
 	}
 
+	return setSha256(d, value)
+}
+
+func setSha256(d *schema.ResourceData, data []byte) error {
 	hasher := sha256.New()
-	return d.Set("sha256", string(hasher.Sum(value)))
+	return d.Set("sha256", string(hasher.Sum(data)))
+}
+
+func getValue(d *schema.ResourceData) ([]byte, error) {
+	if source, ok := d.GetOk("source"); ok {
+		fname, err := home.Expand(source.(string))
+		if err != nil {
+			return nil, err
+		}
+
+		return ioutil.ReadFile(fname)
+	}
+
+	if content, ok := d.GetOk("content"); ok {
+		return []byte(content.(string)), nil
+	}
+
+	if contentB64, ok := d.GetOk("content_base64"); ok {
+		return base64.StdEncoding.DecodeString(contentB64.(string))
+	}
+
+	return nil, fmt.Errorf("source, content, or content_base64 must be set")
+}
+
+func resourceCloudflareWorkersKVCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudflare.API)
+
+	namespaceID := d.Get("namespace_id").(string)
+	key := d.Get("key").(string)
+	value, err := getValue(d)
+	if err != nil {
+		return err
+	}
+
+	if err = setSha256(d, value); err != nil {
+		return err
+	}
+
+	_, err = client.CreateWorkersKV(context.Background(), namespaceID, key, value)
+	return err
+}
+
+func resourceCloudflareWorkersKVUpdate(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceCloudflareWorkersKVDelete(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceCloudflareWorkersKVImport(d *schema.ResourceData, meta interface{}) (result []*schema.ResourceData, err error) {
+	return nil, nil
 }
