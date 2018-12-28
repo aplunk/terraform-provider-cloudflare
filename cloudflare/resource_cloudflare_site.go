444717b6d481cf17dc49304533e6762a4aa6dd5b
Andrew Plunk <aplunk@cloudflare.com>
2018-12-28 17:11:10 -0600

    CFSite: TODO update this message + fix EOF error

--- cloudflare/resource_cloudflare_site.go
+++ cloudflare/resource_cloudflare_site.go
@@ -3,13 +3,14 @@ package cloudflare
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 
 	cloudflare "github.com/cloudflare/cloudflare-go"
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/pkg/errors"
 )
 
 func resourceCloudflareWorkersKVSite() *schema.Resource {
@@ -42,24 +43,32 @@ func resourceCloudflareWorkersKVSite() *schema.Resource {
 type uploader func(key string, value []byte) error
 
 func uploadFile(pathStr, prefix string, info os.FileInfo, split int, uploadKV uploader) (keys []string, err error) {
-	fh, err := os.Open(path.Join(pathStr, info.Name()))
+	fh, err := os.Open(pathStr)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
 	defer fh.Close()
 
 	vSize := int(info.Size())
+	if vSize == 0 {
+		return nil, errors.Errorf("refusing to upload empty file:%s", info.Name())
+	}
+
 	for i := 0; i < vSize; i += split {
 		data := make([]byte, vSize)
 		read, err := fh.ReadAt(data, int64(i))
 		if err != nil {
-			return nil, err
+			log.Printf("read:%d", read)
+			return nil, errors.WithStack(err)
 		}
 
-		key := fmt.Sprintf("%s_%d", prefix, i%vSize)
+		key := prefix
+		if i != 0 {
+			key = fmt.Sprintf("%s_%d", prefix, int(vSize/i))
+		}
 
 		if err := uploadKV(key, data[:read]); err != nil {
-			return nil, err
+			return nil, errors.WithStack(err)
 		}
 		keys = append(keys, key)
 	}
@@ -72,7 +81,7 @@ func uploadSite(namespaceID, source string, limit int, uploadKV uploader) (map[s
 	return largeFiles, filepath.Walk(source, func(pathStr string, info os.FileInfo, err error) error {
 		// fail early if an error is passed in
 		if err != nil {
-			return err
+			return errors.WithStack(err)
 		}
 
 		// unable to upload directories
@@ -81,14 +90,14 @@ func uploadSite(namespaceID, source string, limit int, uploadKV uploader) (map[s
 		}
 
 		// normalize the file key
-		key := fmt.Sprintf("%s_%s", strings.Replace(pathStr, string(filepath.Separator), "_", -1), info.Name())
+		key := strings.Replace(pathStr, string(filepath.Separator), "_", -1)
 
 		// upload large files in chunks returning a mapping of the chunks which will
 		// become a manifest enabling reconstructing the original file
 		if info.Size() > int64(limit) {
 			chunks, err := uploadFile(pathStr, key, info, limit, uploadKV)
 			if err != nil {
-				return err
+				return errors.WithStack(err)
 			}
 			largeFiles[key] = chunks
 			return nil
@@ -96,7 +105,7 @@ func uploadSite(namespaceID, source string, limit int, uploadKV uploader) (map[s
 
 		// files smaller than the limit can be uploaded without returning a mapping
 		_, err = uploadFile(pathStr, key, info, limit, uploadKV)
-		return err
+		return errors.WithStack(err)
 	})
 }
 
--- cloudflare/resource_cloudflare_site_test.go
+++ cloudflare/resource_cloudflare_site_test.go
@@ -1,30 +1,98 @@
 package cloudflare
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
+	"os"
+	"path"
+	"strings"
 	"testing"
 )
 
+type testFile struct {
+	path    []string
+	name    string
+	size    int
+	pathStr string
+	key     string
+	data    []byte
+}
+
+func (t *testFile) make() error {
+	t.pathStr = path.Join(os.TempDir(), path.Join(t.path...))
+	if err := os.MkdirAll(t.pathStr, 0700); err != nil {
+		return err
+	}
+
+	fh, err := ioutil.TempFile(t.pathStr, t.name)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+
+	t.key = strings.Replace(fh.Name(), string(os.PathSeparator), "_", -1)
+
+	t.data = make([]byte, t.size)
+	_, err = fh.Write(t.data)
+	return err
+}
+
+func (t *testFile) cleanup() {
+	os.RemoveAll(t.pathStr)
+}
+
 func TestUploadSite(t *testing.T) {
-	testDir, err := ioutil.TempDir("", "terraform-upload-test")
-	if err != nil {
-		t.Fatal(err)
+	tests := []*testFile{
+		&testFile{
+			[]string{"terraform-site-test"},
+			"one",
+			50,
+			"",
+			"",
+			nil,
+		},
+		&testFile{
+			[]string{"terraform-site-test"},
+			"two",
+			49,
+			"",
+			"",
+			nil,
+		},
+		&testFile{
+			[]string{"terraform-site-test", "nested"},
+			"three",
+			30,
+			"",
+			"",
+			nil,
+		},
 	}
 
-	first, err := ioutil.TempFile(testDir, "one")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	fContent := make([]byte, 30)
-	first.Write(fContent)
-
-	second, err := ioutil.TempFile(testDir, "two")
-	if err != nil {
-		t.Fatal(err)
+	keys := make([]string, len(tests))
+	for i, setup := range tests {
+		if err := setup.make(); err != nil {
+			t.Fatal(err)
+		}
+		defer setup.cleanup()
+		keys[i] = setup.key
 	}
 
 	test := func(key string, value []byte) error {
+		for _, test := range tests {
+			if test.key == key && bytes.Equal(test.data, value) {
+				return nil
+			}
+			t.Logf("received key:%s value:%d", key, len(value))
+			t.Logf("skipping key:%s value:%d", test.key, len(test.data))
+		}
+		t.Fatalf("key:%s value:%d not in %+v", key, len(value), keys)
 		return nil
+	}
+
+	_, err := uploadSite("test_namespace", path.Join(os.TempDir(), "terraform-site-test"), 49, test)
+	if err != nil && err != io.EOF {
+		t.Fatalf("%+v", err)
 	}
 }
